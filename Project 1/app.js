b77e3adb34c6bcd84db3355e0faeecc84ad6ad83
Minerdev1103 <121298556+Minerdev1103@users.noreply.github.com>
2024-03-31 11:35:33 +0700

    start learn

---
+++ Project 1/app.js
@@ -0,0 +1,12 @@
+console.log("Hello Earth ")
+var name = "Earth"
+console.log(name)
+var age = (4+2*2/2*2+4+1-5+19)+91
+alert(age)
+
+var x = 20
+{
+    var x = 100
+    let y = 15
+}
+console.log(x)
