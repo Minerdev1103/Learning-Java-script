4d71697d7fbe4cbfade40c8e615c1276cab92b00
Minerdev1103 <121298556+Minerdev1103@users.noreply.github.com>
2024-04-01 15:00:38 +0700

    อัพเดท
    โปรเจ็คหก

---
+++ project 6/app.js
@@ -0,0 +1,31 @@
+let round = prompt("คุณจะเล่นทั้งหมดกี่รอบ ?")
+let sum = ""
+for(var i=0 ; i <round ; i++){
+    var answer = prompt("หัว หรือ ก้อย พิมพ์มาเลย")
+    var random_answer = ""
+    sum = sum + parseInt(answer)
+    document.getElementById("game-list").innerHTML += 
+    "ทายครั้งที่"+ i +":" +random_answer + "<br>"
+    
+    if(Math.floor(Math.random() *10) <= 4 ){
+        //หัว
+        random_answer = "หัว"
+      
+    }
+    else{
+        //ก้อย
+        random_answer = "ก้อย"
+        
+    }
+
+    if(answer == random_answer){
+        alert("คุณตอบถูกยินดีด้วย !!!")
+    
+    }
+    else{
+        alert("คุณตอบผิด เสียใจด้วย !!!")
+    }
+    console.log(random_answer+" : " + answer)
+    
+}
+document.getElementById("game-list").innerHTML = "ทายถูก" + sum + "ข้อ"
